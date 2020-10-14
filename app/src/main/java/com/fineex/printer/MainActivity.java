5f749d32753cde1f2dd758f3c40875a50ffa70eb
陈熠 <cheny@fineex.com>
2020-10-14 16:01:47 +0800

    增加出库箱唛打印示例

--- app/src/main/java/com/fineex/printer/MainActivity.java
+++ app/src/main/java/com/fineex/printer/MainActivity.java
@@ -32,7 +32,31 @@ public class MainActivity extends AppCompatActivity {
     public void boxCommonMark(View view) {
         printList.clear();
         MarkPrintBean bean = new MarkPrintBean(getResources());
+        bean.setStoreName("T湖北武汉海林广场店");
         bean.setTemplateType("9801");
+        bean.setStoreCode("456565");
+        bean.setBoxNum("1");
+        bean.setConsignee("小明");
+        bean.setConsigneePhone("17601236985");
+        bean.setConsigneeAddress("上海市宝山区呼兰路100号");
+        bean.setSender("小明");
+        bean.setSenderWarehouse("上海发网仓库");
+        bean.setSenderPhone("17601236985");
+        bean.setSenderAddress("上海市宝山区呼兰路100号");
+        bean.setOrderCode("567484154655465");
+        bean.setTransportCode("4554545715454");
+        bean.setWeight("54");
+        bean.setVolume("4");
+        bean.setSKU("5");
+        bean.setCount("56");
+        bean.setSumBoxCount("56");
+        bean.setBoxOrder("6");
+        bean.setRemark("五百万现金");
+        bean.setPrintUser("小明");
+        bean.setPrintDate("2019-01-01 12:00");
+        bean.setOrderDate("2019-01-01");
+        bean.setStoreCodeName("46556564T湖北武汉海林广场店");
+
         printList.add(bean);
 
         PrinterService.startPrintService(this, printList);
@@ -41,7 +65,32 @@ public class MainActivity extends AppCompatActivity {
     public void boxXBLMark(View view) {
         printList.clear();
         MarkPrintBean bean = new MarkPrintBean(getResources());
-        bean.setTemplateType("9802");
+        bean.setStoreName("T湖北武汉海林广场店");
+        bean.setTemplateType("9801");
+        bean.setStoreCode("456565");
+        bean.setBoxNum("1");
+        bean.setConsignee("小明");
+        bean.setConsigneePhone("17601236985");
+        bean.setConsigneeAddress("上海市宝山区呼兰路100号");
+        bean.setSender("小明");
+        bean.setSenderWarehouse("上海发网仓库");
+        bean.setSenderPhone("17601236985");
+        bean.setSenderAddress("上海市宝山区呼兰路100号");
+        bean.setOrderCode("567484154655465");
+        bean.setTransportCode("4554545715454");
+        bean.setWeight("54");
+        bean.setVolume("4");
+        bean.setSKU("5");
+        bean.setCount("56");
+        bean.setSumBoxCount("56");
+        bean.setBoxOrder("6");
+        bean.setRemark("五百万现金");
+        bean.setPrintUser("小明");
+        bean.setPrintDate("2019-01-01 12:00");
+        bean.setOrderDate("2019-01-01");
+        bean.setStoreCodeName("46556564T湖北武汉海林广场店");
+
+        printList.add(bean);
         printList.add(bean);
 
         PrinterService.startPrintService(this, printList);
